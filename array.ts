9e0864699785a3f9bded671515f37a519435aed1
durgarao <asapusirisha@gmail.com>
2020-03-23 16:50:27 +0530

    nested array and multiple types

--- array.ts
+++ array.ts
@@ -5,3 +5,11 @@ let exam:string[]=["sirisha","shivangi","mohsin","Tanmay"];//array with string e
 let x:boolean[]=[true,false];//array with boolean values
 
 let y:any[]=[12,3,13,24,56,42];//array of any type wheather it is string,number,boolean
+
+let z:(number|string)[]=["john","jack","krystle","Remo","Rahgav"];//multiple type array
+
+//nested array
+//3 dimensional array
+let c:number[][][]=[[[1231,2321,31],[3,32,34],[3256,4,23],[14,5.5,1],[1,2,5],[],[3256,4,23],[14,5.5,1],[1,2,5],[]]];//
+//2 dimensional array
+let p:boolean[][]=[[true,false],[false,true]];
