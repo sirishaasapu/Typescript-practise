4238c5ed88fddaf8e458ffa0c51dfa8d0b28144e
durgarao <asapusirisha@gmail.com>
2020-03-23 17:10:33 +0530

    Tuple

---
+++ tuple.ts
@@ -0,0 +1,11 @@
+//Tuple:New datatype it is a set of different datatypes
+//it will store a value of that particular type
+const tup:[number,string]=[21,"sirisha"];//tuple with two datatypes
+
+const value:[string,number,boolean]=["helleo",23,true];//tuple with three datatypes
+
+const val:[number,string,string,number]=[21,"sirisha","asapu",505];
+
+let animal:[string,number];
+animal=["cat",2];
+
