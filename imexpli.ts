65d44d7bf0a5793b539de0a7de0cb5e77601f975
durgarao <asapusirisha@gmail.com>
2020-03-23 14:52:35 +0530

    explict and implict typing

---
+++ imexpli.ts
@@ -0,0 +1,19 @@
+//how we assign a value wheater it is implict or explict 
+ 
+let example="sirishaasapu";//implict type//type of variable not declared
+
+var age:number;
+age=21;//explict typing//type of variable declared
+
+var A=[1,2,3,4,5];//implict typing
+var A:number[]=[1,2,3,4,5];//explict typing
+var X=A.reduce((n1,n2)=>n1+n2);
+
+let example4: undefined = undefined;//explict typing
+
+let example5: null = null;
+
+
+
+
+
