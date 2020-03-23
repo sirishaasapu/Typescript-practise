10f8612dd4808975bbf67e0421ebe5a300196cf5
durgarao <asapusirisha@gmail.com>
2020-03-23 16:34:24 +0530

    Type assertion:cast  variable type as string

---
+++ assert.ts
@@ -0,0 +1,6 @@
+//type asseration or type casting :if don't konw the type of variable you want to make it string
+//we  use ***as string ***to convert or ***<string>***
+let animall :any="i am a dog";
+let anii=(animall as string).length;//here we cast it as a string
+//            (or)
+//let anim=(<string>animall).length;//we can also use <string> cast it as string
