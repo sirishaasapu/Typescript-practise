8da78b20ed04e83df954ebf054f431b9da9edd82
durgarao <asapusirisha@gmail.com>
2020-03-23 14:19:56 +0530

    variable types

---
+++ var.ts
@@ -0,0 +1,7 @@
+let example1 : boolean=true;//boolean variable type
+let example2:number=335;//number variable type
+let example3:string="true";//string variable type
+//any means it will take any type of variables
+let examp:any=123;
+let exampl:any="jxcj";
+let example:any=false;
