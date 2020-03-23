1d419cec3c02dbf253440369433c65fb4c4a2b12
durgarao <asapusirisha@gmail.com>
2020-03-23 16:42:35 +0530

    Array types:number,string,bollean,any

---
+++ array.ts
@@ -0,0 +1,7 @@
+let example:number[]=[1,2,4,343,5,21];//numbered array
+
+let exam:string[]=["sirisha","shivangi","mohsin","Tanmay"];//array with string elements
+
+let x:boolean[]=[true,false];//array with boolean values
+
+let y:any[]=[12,3,13,24,56,42];//array of any type wheather it is string,number,boolean
