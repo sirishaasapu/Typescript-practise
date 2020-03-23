02b74565eabf1238eb2729125c31a21f19d65e4f
durgarao <asapusirisha@gmail.com>
2020-03-23 17:16:41 +0530

    accessing and pushing elemnts to tuple

--- tuple.ts
+++ tuple.ts
@@ -1,11 +1,17 @@
 //Tuple:New datatype it is a set of different datatypes
 //it will store a value of that particular type
 const tup:[number,string]=[21,"sirisha"];//tuple with two datatypes
-
+tup[0];
+tup[1];//accessing
 const value:[string,number,boolean]=["helleo",23,true];//tuple with three datatypes
 
-const val:[number,string,string,number]=[21,"sirisha","asapu",505];
+const val:[number,string,string,number]=[21,"sirisha","asapu",505];//it will allow like this
 
-let animal:[string,number];
-animal=["cat",2];
+let animal:[string,number];//declaration 
+animal=["cat",2];//initialization
+
+//push elements to tuple
+//typescript allow values tp push without delcaration for added values like[string,number] 
+tup.push(23,"shivangi");
+tup.push(29,"mohsin");
 
