dc616f64957a5ee73da5a3bb1b87ffe8190d95f9
durgarao <asapusirisha@gmail.com>
2020-03-23 17:31:11 +0530

    another way to declare array and accessing array

--- array.ts
+++ array.ts
@@ -1,4 +1,11 @@
 let example:number[]=[1,2,4,343,5,21];//numbered array
+//let example:Array<number>=[1,2,4,343,5,21];//another way of array declartion
+
+//accessing array elements
+example[0];
+example[1];
+example[2];
+example[3];
 
 let exam:string[]=["sirisha","shivangi","mohsin","Tanmay"];//array with string elements
 
@@ -7,6 +14,8 @@ let x:boolean[]=[true,false];//array with boolean values
 let y:any[]=[12,3,13,24,56,42];//array of any type wheather it is string,number,boolean
 
 let z:(number|string)[]=["john","jack","krystle","Remo","Rahgav"];//multiple type array
+//another way of declaring multi type array
+//let z:Array<number|string>=["john","jack","krystle","Remo","Rahgav"];
 
 //nested array
 //3 dimensional array
