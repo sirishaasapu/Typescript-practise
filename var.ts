00d0c936298431825be762246954c472b4a87b79
durgarao <asapusirisha@gmail.com>
2020-03-24 15:19:50 +0530

    Type notation in function parameters and objects

--- var.ts
+++ var.ts
@@ -5,3 +5,17 @@ let example3:string="true";//string variable type
 let examp:any=123;
 let exampl:any="jxcj";
 let example:any=false;
+//function parameters
+function fun(a:number,b:number)
+{
+    return a+b;
+}
+//type notation in objects
+var emp:{
+    id:number;
+    ename:string;
+};
+emp={
+    id:505,
+    ename:"sirisha"
+};
