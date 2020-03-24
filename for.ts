4241f0673032c49a2ad0a5b4b33c9c22a7828548
durgarao <asapusirisha@gmail.com>
2020-03-24 16:38:20 +0530

    for of loop

--- for.ts
+++ for.ts
@@ -3,3 +3,10 @@ for(let i:number=0;i<=n.length;i++)
 {
     console.log("n");
 }
+
+//for of loop
+let fruit:string[]=["apple","banana","kiwi","orange"];
+let c;
+for (c of fruit){
+ console.log(c);
+} 
