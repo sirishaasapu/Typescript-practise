9f7092b0ec6df9e9d4debe91c4c2ad6fcc0aeff4
durgarao <asapusirisha@gmail.com>
2020-03-24 15:40:15 +0530

    numeric enum

---
+++ enum.ts
@@ -0,0 +1,37 @@
+//Enum:new datatype : enums allow us to declare a set of named constants i.e. a collection of related values that can be numeric or string values.
+//3 types 
+//1.numeric enum
+/*Numeric enums are number-based enums i.e. they store string values as numbers. */
+
+//start with enum 0 value
+enum cost{
+     gold,
+     sliver,
+     platinum,
+     alluminum
+}
+cost[0];//gold
+cost[1];//sliver
+cost[3];//alluminum
+
+//start with enum 1 
+
+enum id
+{
+    id=1,
+    id1,
+    id2
+}
+id[1];//id
+id[2];//id1
+id[2];//id2
+
+
+enum fruits{
+    apple=300,
+    banana=50,
+    grapes=100,
+    orange=200
+}
+fruits[300];//apple
+  
