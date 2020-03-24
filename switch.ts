7ac24b076392228583f3ac70615792bf97e21ddc
durgarao <asapusirisha@gmail.com>
2020-03-24 16:30:35 +0530

    switch case

---
+++ switch.ts
@@ -0,0 +1,19 @@
+let day:number=3;
+switch(day)
+{
+    case 1:
+         console.log("monday");break;
+    case 2:
+         console.log("Tuesday");break;
+    case 3:
+        console.log("wednesday");break;
+    case 4:
+        console.log("Thrsday");break;
+    case 5:
+        console.log("friday");break;
+    case 6:
+          console.log("saturday");break;
+    default:
+    console.log("no such day exists");break;
+
+}
