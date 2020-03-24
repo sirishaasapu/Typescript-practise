c0fe4322327685070e37233bf9ce83e62569fce2
durgarao <asapusirisha@gmail.com>
2020-03-24 15:56:20 +0530

    union :multi types

--- multi.ts
+++ multi.ts
@@ -2,3 +2,12 @@ let x :number|string="asnasbasd";//we can declare multiple type of variables lik
 let y:boolean|number=false;
 let z:any=2168173;
 const a:boolean|string="ahsdab";
+
+//function of multi types :union
+function fun(a:(string|number))
+{
+        console.log(a);
+}
+fun(123);
+fun("sirisha");
+//fun(true);//it is an error
