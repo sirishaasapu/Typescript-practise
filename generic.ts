7d1a0131ca8cf1961e65d1f491d846fa661cd239
durgarao <asapusirisha@gmail.com>
2020-04-08 14:53:42 +0530

    generic function

---
+++ generic.ts
@@ -0,0 +1,7 @@
+//generic function
+//used to manipulate datatype
+function genericc<T,U>(name:T,number:U):void{
+    console.log("name" +name+"number"+number);
+}
+genericc<string,number>("sirisha",635452);
+genericc<number,number>(24625,662347);
